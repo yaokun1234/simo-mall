1b1fc962bcc28d796bd542fcb883d59fab665cdf
simo <2921847833@qq.com>
2022-04-13 23:39:38 +0800

    项目初始结构创建

---
+++ mall-order/src/main/java/com/simo/mallorder/MallOrderApplication.java
@@ -0,0 +1,13 @@
+package com.simo.mallorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallOrderApplication.class, args);
+    }
+
+}
---
+++ mall-order/src/test/java/com/simo/mallorder/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simo.mallorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
